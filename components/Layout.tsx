60a7f50466a4ad16990d4bed9ca302fd64f37853
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2022-08-10 16:15:30 +0200

    feat: favicon added

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,14 @@
 import {PropsWithChildren} from 'react';
 import SideBar from './SideBar';
+import Head from "next/head";
 
 interface IProps extends PropsWithChildren {}
 
-const Layout = (props: IProps) => {
-    return <SideBar>{props.children}</SideBar>
-}
+const Layout = (props: IProps) => <>
+    <Head>
+        <link rel="icon" href="/images/favicon.png" />
+    </Head>
+    <SideBar>{props.children}</SideBar>
+</>
 
 export default Layout;
