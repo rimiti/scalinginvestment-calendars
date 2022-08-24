ec24b17857724ab9b3053b97b1b34ce8c39e6e2b
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2022-08-24 13:55:29 +0200

    feat: calendars updated

--- pages/ozoir-la-ferriere/rdc/studio-f2/index.tsx
+++ pages/ozoir-la-ferriere/rdc/studio-f2/index.tsx
@@ -6,7 +6,7 @@ const OzoirLaFerriereRdcStudioF2 = () => {
         <Head>
             <title>Calendrier</title>
         </Head>
-        <Calendar url="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FParis&showTitle=0&title=Ozoir%20-%20RDC%20-%20Studio%20F2&showNav=1&showDate=1&showPrint=0&src=ZGd1bGdiZjAyZ2Y2M2phZTYwZ21pcDFwOXVhcGdvNTZAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=aWd2MWo1bnJwamJyYmxycDJxczBranN1bjQ5Y3RicnBAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%230B8043&color=%230B8043"/>
+        <Calendar url="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FParis&showTitle=0&title=Ozoir%20-%20RDC%20-%20Studio%20F2&showNav=1&showDate=1&showPrint=0&src=MTRiaGc5bnJ0amFqa2xmYTdpbXY4NjMzajhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=aGhmZnNlNDl0cWFkZWk5aTM1bzY1aTcydW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%230B8043&color=%230B8043"/>
     </>
 }
 
