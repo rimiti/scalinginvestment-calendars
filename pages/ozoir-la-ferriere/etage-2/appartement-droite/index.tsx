082c13d38751a14d7d8fde71095ae40bae50f3ac
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2022-09-28 17:51:51 +0200

    feat: new appartment added

--- pages/ozoir-la-ferriere/etage-2/appartement-droite/index.tsx
+++ pages/ozoir-la-ferriere/etage-2/appartement-droite/index.tsx
@@ -4,7 +4,7 @@ import Calendar from "../../../../components/Calendar";
 const OzoirLaFerriereAppartementEtage2Droite = () => {
     return <>
         <Head>
-            <title>Calendrier</title>
+            <title>Calendrier: 2E - Appartement D</title>
         </Head>
         <Calendar url="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FParis&showTitle=0&title=Ozoir%20-%20RDC%20-%20Studio%20F2&showNav=1&showDate=1&showPrint=0&src=ZWloN3VzbTdocjdkcm10YnR2ZWtrajdwYThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=Z2xncDRkMG5vNmdnZ3Jpc21yZjYzYW0wNzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%230B8043&color=%230B8043"/>
     </>
--- pages/ozoir-la-ferriere/rdc/studio-f2/index.tsx
+++ pages/ozoir-la-ferriere/rdc/studio-f2/index.tsx
@@ -4,7 +4,7 @@ import Calendar from "../../../../components/Calendar";
 const OzoirLaFerriereRdcStudioF2 = () => {
     return <>
         <Head>
-            <title>Calendrier</title>
+            <title>Calendrier: RDC - Studio F2</title>
         </Head>
         <Calendar url="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FParis&showTitle=0&title=Ozoir%20-%20RDC%20-%20Studio%20F2&showNav=1&showDate=1&showPrint=0&src=MTRiaGc5bnJ0amFqa2xmYTdpbXY4NjMzajhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=aGhmZnNlNDl0cWFkZWk5aTM1bzY1aTcydW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%230B8043&color=%230B8043"/>
     </>
